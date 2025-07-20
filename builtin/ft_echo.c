890f79b6e22833d7472dbc1b12969026fc9c2aed
e-mcb <maximezutter@gmail.com>
2025-07-20 23:13:45 +0200

    echo ok

--- builtin/ft_echo.c
+++ builtin/ft_echo.c
@@ -70,8 +70,9 @@ int	ft_echo(char **str, t_shell *shell, int exec_size, int fd_out)
 	{
 		if (ft_putstr_fd_echo(str[i], exec_size, fd_out) == 1)
 			return (1);
-		if (str[i + 1] && ft_strlen(str[i]) != ' ')
-			ft_putstr_fd(" ", 1);
+		if (str[i + 1] && (ft_strlen(str[i]) == 0
+			|| str[i][ft_strlen(str[i]) - 1] != ' '))
+			ft_putstr_fd(" ", fd_out);
 		i++;
 	}
 	if (newline)
